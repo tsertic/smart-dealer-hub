768949dfad953e4723e777e7711f7c4c4b7250e7
tsertic <tsertic5@gmail.com>
2025-04-07 10:22:50 +0200

    middleware inital setup,auth pages skeletons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/layout/Footer";
-
+import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -20,11 +20,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${inter.variable}`}>
-      <body className="flex flex-col min-h-screen antialiased font-sans">
-        <main className="flex-grow">{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className={`${inter.variable}`}>
+        <body className="flex flex-col min-h-screen antialiased font-sans">
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
 import { Button } from "@/components/ui/button";
-
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="">
+      <header className="flex justify-end items-center p-4 gap-4 h-16">
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
       <p>Subscribe to Smart dealer Hub</p>
       <Button>Subscribe</Button>
     </div>
